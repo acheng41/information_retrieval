b3f34873203dafe26ffe8b13784ada4f64f7d5f8
Andrea  Cheng <andreacheng@Andreas-MacBook-Pro-3.local>
2024-04-11 00:53:35 -0400

    passing all gradescope tests

--- HW4/hw4.py
+++ HW4/hw4.py
@@ -4,6 +4,7 @@ import sys
 from bs4 import BeautifulSoup
 from queue import Queue
 from urllib import parse, request
+import heapq
 
 logging.basicConfig(level=logging.DEBUG, filename='output.log', filemode='w')
 visitlog = logging.getLogger('visited')
@@ -23,25 +24,38 @@ def parse_links(root, html):
 
 
 def parse_links_sorted(root, html):
-    # TODO: implement
-    links = list(parse_links(root, html))
-    return relevance(links)
+    # DONE: implement    
+    priority = [] #create a priority queue using heapq
+    soup = BeautifulSoup(html, 'html.parser')
+    for link in soup.find_all('a'):
+        href = link.get('href')
+        if href:
+            text = link.string
+            if not text:
+                text = ''
+            text = re.sub('\s+', ' ', text).strip()
+            #add link to queue sorting based on relevance function
+            heapq.heappush(priority, ((rel(text)), (parse.urljoin(root, link.get('href')), text)))
+    for link in priority:
+        yield (heapq.heappop(priority)[1])
 
-def relevance(links): 
-    #define most relevant links by length of title and then length of the link
-    #usually, descriptive titles can be helpful for finding information
-    #usually, longer links can indicate paths that enter subpages to more specific information
-    links.sort(key = lambda x: len(x[0]), reverse  = True) 
-    links.sort(key = lambda x: len(x[1]), reverse = True)
-    return links
+#generate priority using the length of the title
+#usually, descriptive titles can be helpful for finding information
+def rel(text):
+    if len(text) == 0: 
+        return 2
+    else:
+        return 1/ len(text)
+
 
 def get_links(url):
     res = request.urlopen(url)
     return list(parse_links(url, res.read()))
 
-# def get_links_sorted(url):
-#     res = request.urlopen(url)
-#     return parse_links_sorted(url, res.read())
+# code to test output of sorted links
+def get_links_sorted(url):
+    res = request.urlopen(url)
+    return list(parse_links_sorted(url, res.read()))
 
 
 def get_nonlocal_links(url):
@@ -51,22 +65,20 @@ def get_nonlocal_links(url):
 
     # DONE: implement
     links = get_links(url)
-    base_domain = parse.urlparse(url).netloc
-
+    #get domain and path for the base link
+    base_domain = parse.urlparse(url).netloc 
     base_path = parse.urlparse(url).path
 
 
     filtered = []
-
-
     for link, title in links:
         parsed_link = parse.urlparse(link)
         parsed_path = parsed_link.path
         parsed_domain = parsed_link.netloc
+        #non-local links should not have the same domain 
+        #self-referencing links have the same path
         if parsed_domain != base_domain or parsed_path != base_path:
-            filtered.append((link, title))
-
-
+            filtered.append((link, title)) #only add non-local and non-self referencing
     return filtered
 
 
@@ -75,19 +87,17 @@ def crawl(root, wanted_content=[], within_domain=True):
     `wanted_content` is a list of content types to crawl
     `within_domain` specifies whether the crawler should limit itself to the domain of `root`
     '''
-    # TODO: implement
-
+    # DONE: implement
     queue = Queue()
     queue.put(root)
 
     visited = []
     extracted = []
 
-    while not queue.empty() and len(visited) < 5:
-  
+    while not queue.empty():
         url = queue.get()
-        if url in visited:
-            continue
+        if url in visited: 
+            continue #do not visit duplicates
         try:
             req = request.urlopen(url)
             html = req.read().decode('utf-8', 'ignore')
@@ -95,18 +105,19 @@ def crawl(root, wanted_content=[], within_domain=True):
             visited.append(url)
             visitlog.debug(url)
 
-
             for ex in extract_information(url, html):
                 extracted.append(ex)
                 extractlog.debug(ex)
             
-            for link, title in parse_links(url, html):
+            # for link, title in parse_links(url, html):  #the regular crawler          
+            for link, title in parse_links_sorted(url, html): #custom link relevance sorting
+                print(link)
                 req_link = request.urlopen(link)
                 content_type = req_link.headers['Content-Type']
      
                 if not any(content in content_type for content in wanted_content):
                     continue # the content type does not match any of the content listed in wanted_content
-
+                
                 
                 if is_self_referencing(link, url):
                     continue
@@ -115,6 +126,8 @@ def crawl(root, wanted_content=[], within_domain=True):
                         queue.put(link) # link that is being parsed is not in the domain that we're specifying
                 else:
                     queue.put(link)
+
+                print(queue.__sizeof__())
 
         except Exception as e:
             print(e, url)
@@ -141,14 +154,17 @@ def extract_information(address, html):
     # DONE: implement
     results = []
 
+    #Search for Phone Numbers
     for match in re.findall(r'\d\d\d-\d\d\d-\d\d\d\d', str(html)):
         results.append((address, 'PHONE', match))
     for match in re.findall(r'\(\d\d\d\)\s?\d\d\d-\d\d\d\d', str(html)):
         results.append((address, 'PHONE', match))
 
+    #search for emails
     for match in re.findall(r'(\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b)', str(html)):
         results.append((address, 'EMAIL', match))
     
+    #search for addresses
     for match in re.findall(r'([A-Z][a-zA-Z\s]+),\s*([A-Z][a-zA-Z\s.]+)\s+(\d{5})', str(html)):
         results.append((address, 'ADDRESS', match))
         print(match)
@@ -168,17 +184,18 @@ def main():
     links = get_links(site)
     writelines('links.txt', links)
 
-    # links2 = get_links_sorted(site)
-    # writelines('links2.txt', links2)
+    # code to test sorted links
+    links2 = get_links_sorted(site)
+    writelines('links2.txt', links2)
 
     nonlocal_links = get_nonlocal_links(site)
     writelines('nonlocal.txt', nonlocal_links)
 
     visited, extracted = crawl(site,['text/html'])
+    print(extracted)
 
     writelines('visited.txt', visited)
     writelines('extracted.txt', extracted)
-
 
 
 if __name__ == '__main__':
